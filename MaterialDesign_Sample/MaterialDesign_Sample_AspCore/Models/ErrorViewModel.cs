437e0821b8713b7f9c0adff52a0da861acf33bdd
sadra <sadra@DESKTOP-4HQA28O>
2020-03-04 12:42:41 +0330

    init AspCore Sample

---
+++ MaterialDesign_Sample/MaterialDesign_Sample_AspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MaterialDesign_Sample_AspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
